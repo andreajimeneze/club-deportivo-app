cc4e68846730074018f5738d33a904d21b405729
Andrea Jiménez Espinoza <andreacjimenez@gmail.com>
2026-06-06 00:07:50 -0400

    Revert "elimina clase Conexion"
    This reverts commit b6e1da9a06726f8f6e2cc39f255beeaf9b1729f2.

---
+++ BaseDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace ClubDeportivoApp
+{
+    public class Conexion
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
