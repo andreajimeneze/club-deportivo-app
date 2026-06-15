7b8215922a60f871ac503459e193bbd7e2da657a
ramirorm865 <menicaramiro@gmail.com>
2026-06-14 21:22:02 -0300

    Mis cambios locales antes del pull con listado de vencimietos

--- Formularios/VencimientosForm.cs
+++ Formularios/VencimientosForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClubDeportivoApp.Repositorios;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,35 +14,60 @@ namespace ClubDeportivoApp.Formularios
     public partial class VencimientosForm : Form
     {
         private readonly ConexionMySql _conexion;
+        private VencimientoRepo _vencimientoRepo;
+
         public VencimientosForm(ConexionMySql conexion)
         {
             InitializeComponent();
             _conexion = conexion;
+            _vencimientoRepo = new VencimientoRepo(conexion);
+            lblFechaHoy.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+
+            
+            this.Load += VencimientosForm_Load;
+            btnBuscarVencimientos.Click += btnBuscar_Click;
+
+            
+            dataGridView1.AutoGenerateColumns = true;
+           
+            dataGridView1.Columns.Clear();
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void VencimientosForm_Load(object sender, EventArgs e)
         {
-
+            CargarVencimientos(DateTime.Today);
         }
 
+        private void CargarVencimientos(DateTime fecha)
+        {
+            try
+            {
+                DataTable dt = _vencimientoRepo.ObtenerVencimientosPorFecha(fecha);
+                dataGridView1.DataSource = dt;
+                lblCantidad.Text = $"Total: {dt.Rows.Count} socios con vencimiento en {fecha:dd/MM/yyyy}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar vencimientos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = dtpFecha.Value;
+            CargarVencimientos(fecha);
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e) => this.Close();
+        private void btnMinimizar_Click(object sender, EventArgs e) => this.WindowState = FormWindowState.Minimized;
+        private void btnCerrar_Click(object sender, EventArgs e) => this.Close();
         private void button3_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void btnVolver_Click(object sender, EventArgs e)
+        private void dataGridView1_CellContentClick(object sender, EventArgs e)
         {
-            this.Close();
-        }
 
-        private void btnMinimizar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void btnCerrar_Click(object sender, EventArgs e)
-        {
-            this.Close();
         }
     }
 }
